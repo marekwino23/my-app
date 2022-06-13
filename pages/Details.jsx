ada39c5f82fd192293f15fd7520b65a35e414b40
marekwino23 <marekw1996@gmail.com>
2022-06-13 15:10:02 +0200

    setup Details and Home components

---
+++ pages/Details.jsx
@@ -0,0 +1,12 @@
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+export default DetailsScreen;
